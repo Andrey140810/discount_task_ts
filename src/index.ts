f4c9fa772118246d4ffaa705c4bf6a017ebf9c86
Andrey <krepezh_plus@mail.ru>
2026-04-04 00:10:49 +0500

    complete discount task

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+interface TotalPrice {
+  price: number;
+  discount: number;
+  isInstallment: boolean;
+  months?: number;
+}
+
+const totalPrice = ({
+  price,
+  discount,
+  isInstallment,
+  months,
+}: TotalPrice): number => {
+  if (isInstallment && months) {
+    return (price - (price * discount) / 100) / months;
+  } else {
+    return price - (price * discount) / 100;
+  }
+};
+
+const price = totalPrice({
+  price: 100000,
+  discount: 25,
+  isInstallment: true,
+  months: 12,
+});
+console.log(price); // 6250
